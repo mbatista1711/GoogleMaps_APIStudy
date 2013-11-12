cc78e80cf95139ead74e7dfcae1d4c1daf5c14e8
Gabriel Garcia Ferraz do Amaral <gabriel_a7x@hotmail.com>
2013-11-12 17:46:45 -0200

    Commit inicial

---
+++ js/mapa.js
@@ -0,0 +1,70 @@
+var map;
+var idInfoBoxAberto;
+var infoBox = [];
+var markers = [];
+
+function initialize() {	
+	var latlng = new google.maps.LatLng(-23.556884, -46.658968);
+	
+    var options = {
+        zoom: 14,
+		center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("mapa"), options);
+}
+
+initialize();
+
+function abrirInfoBox(id, marker) {
+	if (typeof(idInfoBoxAberto) == 'number' && typeof(infoBox[idInfoBoxAberto]) == 'object') {
+		infoBox[idInfoBoxAberto].close();
+	}
+
+	infoBox[id].open(map, marker);
+	idInfoBoxAberto = id;
+}
+
+function carregarPontos() {
+	
+	$.getJSON('js/pontos.json', function(pontos) {
+		
+		var latlngbounds = new google.maps.LatLngBounds();
+		
+		$.each(pontos, function(index, ponto) {
+			
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
+				icon: 'img/marcador.png'
+			});
+			
+			var myOptions = {
+				content: "<p>" + ponto.Descricao + "</p>",
+				pixelOffset: new google.maps.Size(-150, 0)
+        	};
+
+        	var contentString = "<iframe width='250' height='167' src='http://www.youtube.com/embed/eLRlHcCiNjg' frameborder='0' ></iframe>";
+
+			infoBox[ponto.Id] = new InfoBox(myOptions);
+			infoBox[ponto.Id].marker = marker;
+			
+			infoBox[ponto.Id].listener = google.maps.event.addListener(marker, 'click', function (e) {
+				abrirInfoBox(ponto.Id, marker);
+			});
+			
+			markers.push(marker);
+			
+			latlngbounds.extend(marker.position);
+			
+		});
+		
+		var markerCluster = new MarkerClusterer(map, markers);
+		
+		map.fitBounds(latlngbounds);
+		
+	});
+	
+}
+
+carregarPontos();
